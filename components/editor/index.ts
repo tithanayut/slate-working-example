d791ff273f9b0d0addb24a4f12fa48ee428bd1d1
Thanayut T <b@thanayut.in.th>
2022-07-07 01:17:29 +0700

    Change editor components to barrel export

---
+++ components/editor/index.ts
@@ -0,0 +1,3 @@
+export { default as RichTextEditor } from "./RichTextEditor";
+export { default as useRichTextEditor } from "./hooks/useRichTextEditor";
+export { blankValue, sampleValue } from "./constants";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
-import RichTextEditor from "../components/editor";
-import useRichTextEditor from "../components/editor/hooks/useRichTextEditor";
-import { blankValue } from "../components/editor/constants";
+import { RichTextEditor, useRichTextEditor, blankValue } from "../components/editor";
 
 const Home: NextPage = () => {
   const { content, onContentChange } = useRichTextEditor(blankValue);
